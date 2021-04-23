4b31e67d3fd935740ab8adc0716b22f73e9dca37
Felipe-Estay <81047527+Felipe-Estay@users.noreply.github.com>
2021-04-23 13:54:55 -0400

    V1

---
+++ JS/scrip.js
@@ -0,0 +1,19 @@
+function mensaje() {
+    alert('hola mundo');
+  }
+  
+  function ValidarFormu(){
+      
+  }
+  function Validapass(){
+  var pass1 = document.getElementById('txtPassword').value;
+  var pass2 = document.getElementById('txtPassword2').value;
+  }
+  
+  /*Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your work has been saved',
+          showConfirmButton: false,
+          timer: 1500
+        })*/
